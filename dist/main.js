de3728bc5a997515f8533fde8526fad4ed4eb375
Wellington Siqueira <wellingtonsiqueira.santos@gmail.com>
2023-06-08 17:48:49 -0300

    Exercício ES6

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+const arrayAlunos = new Array();
+
+arrayAlunos.push({ nome: "wellington", nota: 9 }, { nome: "caroline", nota: 10 }, { nome: "boris", nota: 3 }, { nome: "isabela", nota: 6 }, { nome: "Loana", nota: 5 });
+
+console.log(arrayAlunos);
+
+const alunosAprovados = arrayAlunos.filter(aluno => {
+    return aluno.nota >= 6;
+});
+
+console.log(alunosAprovados);
